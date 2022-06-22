64948944ea06e49d4899106316c649bd1aab52cb
user <user@users-MBP.lan>
2022-06-22 18:24:43 +0100

    Initial commit

---
+++ models/traveller.js
@@ -0,0 +1,37 @@
+const Traveller = function (journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function () {
+  return this.journeys.map(journey => journey.startLocation)
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journey.endLocation)
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter(journey => journey.transport === transport)
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  })
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0)
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  modeOfTransport = this.journeys.map(journey => journey.transport);
+  return modeOfTransport.filter((transport, index, map) => map.indexOf(transport) === index)
+
+}
+
+
+module.exports = Traveller;
